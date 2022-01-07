94470e8ea76b8c216302fb5c3f774c74e31718f2
larissakoliveira <oliveir5uwm@gmail.com>
2022-01-07 15:41:29 -0300

    fix:get all files working now

--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,7 +1,7 @@
 import os
+from flask import Flask, jsonify
 from flask.helpers import safe_join
 from werkzeug.datastructures import FileStorage
-from datetime import datetime, timezone
 from os import getenv
 
 files_directory = getenv('FILES_DIRECTORY')
@@ -23,30 +23,46 @@ def save_image(file: FileStorage, file_extension: str):
     return filename
 
 
-def list_all(extension):
+def get_files():
+    extension_folders = []
+    directory = os.walk(f'./{files_directory}')
 
-    list_all_images = []
+    for _,_,each in directory:
+        extension_folders.append(each)
+
+    return jsonify(
+        jpg=extension_folders[0],
+        gif=extension_folders[1],
+        png=extension_folders[2]
+    )
+
+
+def list_by_extension(extension):
+
+    list_all_images = list()
+
+    # for extent in allowed_extensions:
+    #     final_dir = os.listdir(f'{files_directory}/{extent}
+    #     list_all_images.append(final_dir)
+
     files_list = os.listdir(f'{files_directory}/{extension}')
 
     if extension == 'jpg':
-        list_all_images.append(files_list)
         return files_list
 
     if extension == 'png':
-        list_all_images.append(files_list)
         return files_list
 
     if extension == 'gif':
-        list_all_images.append(files_list)
         return files_list
 
-    return list_all_images
 
 
 def get_path(filename: str, extension: str):
     path = safe_join(files_directory,extension,filename)
 
     return path
+
 
 def zip_images():
     ...
